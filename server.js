cd303c67042d2615559e015523eb8f850f30d91e
Mohamed <mohamed.rmadan18@outlook.com>
2020-11-21 09:25:01 +0200

    greeting message

--- server.js
+++ server.js
@@ -1,51 +1,23 @@
-const express = require('express');
+const express = require('express')
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
-
 const app = express();
 
-//app configuration 
+// app configuration
 app.set('port', (process.env.PORT || 5000));
 
-//setup our express app
-app.use(morgan('dev')); //log every request to console
+
+// setup our express application
+app.use(morgan('dev')); // log every request to the console.
 app.use(bodyParser.urlencoded({ extended:false }));
-app.use(bodyParser.json());
+app.use(bodyParser.json()); 
 
 
-//app routes
+// app routes
 require('./routes/webhook_verify')(app);
 
-//server start
+// warming up the engines !! setta !! go !!!.
 app.listen(app.get('port'), function() {
-    const url = 'http://localhost:' + app.set('port');
-    console.log('app running on port: ' + app.get('port'));
-})
-
-/*
-var express = require("express");
-var request = require("request");
-var bodyParser = require("body-parser");
-
-var app = express();
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
-app.listen((process.env.PORT || 5000));
-
-// Server index page
-app.get("/", function (req, res) {
-  res.send("Deployed!");
+  const url = 'http://localhost:' + app.set('port');
+  console.log('Application running on port: ', app.get('port'));
 });
-
-// Facebook Webhook
-// Used for verification
-app.get("/webhook", function (req, res) {
-  if (req.query["hub.verify_token"] === "this_is_my_token") {
-    console.log("Verified webhook");
-    res.status(200).send(req.query["hub.challenge"]);
-  } else {
-    console.error("Verification failed. The tokens do not match.");
-    res.sendStatus(403);
-  }
-});
-*/
