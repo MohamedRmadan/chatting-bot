5ee31e95e3c79d3c5dff9b1090fc92f99461325f
Mohamed <mohamed.rmadan18@outlook.com>
2020-11-21 08:17:48 +0200

    updating

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = require('express');
+/*const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
@@ -21,3 +21,30 @@ app.listen(app.get('port'), function() {
     const url = 'http://localhost:' + app.set('port');
     console.log('app running on port: ' + app.get('port'));
 })
+*/
+
+var express = require("express");
+var request = require("request");
+var bodyParser = require("body-parser");
+
+var app = express();
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.listen((process.env.PORT || 5000));
+
+// Server index page
+app.get("/", function (req, res) {
+  res.send("Deployed!");
+});
+
+// Facebook Webhook
+// Used for verification
+app.get("/webhook", function (req, res) {
+  if (req.query["hub.verify_token"] === "this_is_my_token") {
+    console.log("Verified webhook");
+    res.status(200).send(req.query["hub.challenge"]);
+  } else {
+    console.error("Verification failed. The tokens do not match.");
+    res.sendStatus(403);
+  }
+});
